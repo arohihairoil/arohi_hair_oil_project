a8ba2ccef6cf47d642ed99294975cc989a2e9f3a
Busapalli Nandiswar <nandiswarb93@gmail.com>
2025-12-28 16:37:13 +0530

    arohi_hair_oil_project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react' // ✅ ADD
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ShopContextProvider>
       <App />
+      <SpeedInsights />
     </ShopContextProvider>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
