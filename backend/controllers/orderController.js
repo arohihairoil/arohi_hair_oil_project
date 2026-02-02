61c8254dd4798d5a8e105d6d932916d224047a83
arohihairoil <arohihairoilsra@gmail.com>
2026-02-02 20:31:34 +0530

    Update orderController.js
    added limit to 200

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -313,7 +313,7 @@ const verifyRazorpay = async (req, res) => {
 const allOrders = async (req, res) => {
   try {
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 50;
+    const limit = Number(req.query.limit) || 200;
     const skip = (page - 1) * limit;
 
     const orders = await orderModel
